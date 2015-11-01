5f1e41543c3fb6e14e6eeff5e2c498dc881832bb
Yogesh Garg <yogeshg91@gmail.com>
2015-11-02 01:06:49 +0530

    making funcitons

--- p3/getPlaces.py
+++ p3/getPlaces.py
@@ -26,63 +26,62 @@ app_param={
 
 loc_param={
 	'location':'0,0',
-	'radius':'10'
+	'radius':'10',
+	'city':'temp'
 };
-locs_param=[
-{
+locs_param=[{
 	'location':'18.9750,72.8258',
 	'radius':'10000',
 	'city':'Mumbai'
-}
-]
-;
+	},
+	{
+	'location':'40.7127,-74.0059',
+	'radius':'10000',
+	'city':'NewYork'
+	}];
 
 
 # Script
 
 # Get and Save places (loc_param)
-
-places=list();
-loc_param.update(app_param);
-iter_param=dict(app_param);
-resp=requests.get(url,params=loc_param).text;
-
-while(len(places)<NUM_PLACES):
-	try:
-		j=json.loads(resp)
-		places.extend([{'placeid':p['place_id'],'name':p['name'],'city':'temp'} for p in j['results']])
-		print len(places)
-		time.sleep(1)
-		iter_param.update({'pagetoken':j['next_page_token']})
-		resp=requests.get(url,params=iter_param).text
-	except Exception, e:
-		print e
-		break
-
-table2csv(places,'places.csv')
-
-# save places
-
-reviews=list();
-iter_param=dict(app_param);
-
-for p in places:
-	try:
-		time.sleep(1)
-		iter_param.update(p)
-		resp=requests.get(url_details,params=iter_param).text
-		j=json.loads(resp)
-		reviews.extend([{ 'placeid':iter_param['placeid'],'review':r['text']} for r in j['result']['reviews'] ])
-		print len(reviews)
-	except Exception, e:
-		print e
-
-
-table2csv(reviews,'reviews.csv')
-# save reviews
+def getAndSaveplaces (loc_param):
+	places=list();
+	loc_param.update(app_param);
+	iter_param=dict(app_param);
+	resp=requests.get(url,params=loc_param).text;
+	
+	while(len(places)<NUM_PLACES):
+		try:
+			j=json.loads(resp)
+			places.extend([{'placeid':p['place_id'],'name':p['name'],'city':loc_param['city']} for p in j['results']])
+			print len(places)
+			time.sleep(1)
+			iter_param.update({'pagetoken':j['next_page_token']})
+			resp=requests.get(url,params=iter_param).text
+		except Exception, e:
+			print e
+			break
+	
+	table2csv(places,'places_'+loc_param['city']+'.csv')
+	
+	reviews=list();
+	iter_param=dict(app_param);
+	for p in places:
+		try:
+			time.sleep(1)
+			iter_param.update(p)
+			resp=requests.get(url_details,params=iter_param).text
+			j=json.loads(resp)
+			reviews.extend([{ 'placeid':iter_param['placeid'],'review':r['text']} for r in j['result']['reviews'] ])
+			print len(reviews)
+		except Exception, e:
+			print e
+	table2csv(reviews,'reviews_'+loc_param['city']+'.csv')
 
 
 
+getAndSaveplaces(locs_param[0])
+getAndSaveplaces(locs_param[1])
 
 
 
--- p3/getPlots.py
+++ p3/getPlots.py
@@ -1,4 +1,7 @@
-import csv, nltk
+import csv, nltk, numpy
+
+import matplotlib.pyplot as plt
+
 
 def csv2table(csvfile):
 	table=list()
@@ -28,46 +31,40 @@ def getTopQuantileIdx(fq,q):
 			break
 	return i
 
+def save_plot(p_words_fd,n_words_fd,cname):
+	plt.bar(numpy.arange(len(p_words_fd)), [w[1] for w in p_words_fd], 1, color='g',alpha=0.25)
+	plt.bar(numpy.arange(len(n_words_fd)), [w[1] for w in n_words_fd], 1, color='r',alpha=0.25)
+	[plt.text(i,p_words_fd[i][1],p_words_fd[i][0]) for i in range(len(p_words_fd))]
+	[plt.text(i,n_words_fd[i][1],n_words_fd[i][0]) for i in range(len(n_words_fd))]
+	# plt.show()
+	plt.savefig('plot_'+cname+'.png')
 
-rs=csv2table('reviews.csv')
-
-txt=[r['review'] for r in rs]
-
-tagd=[nltk.pos_tag(nltk.word_tokenize(t)) for t in txt]
-
-tags=[tag for sl in tagd for (txt,tag) in sl]
-f1=nltk.FreqDist(tags)
-f1.plot()
-f1.tabulate()
-
-jjs=[txt for sl in tagd for (txt,tag) in sl if tag==('JJ')]
-f2=nltk.FreqDist(jjs)
-f2.plot()
-f2.tabulate()
-
-ws={}
-for item in kvpairs2table('words.tff'):
-   word1 = item.pop('word1')
-   ws[word1] = item
-
-f2_half=f2.items()[0:getTopQuantileIdx(f2,0.5)]
-
-p_words_fd=([w for w in f2_half if (w[0] in ws.keys()) and ws[w[0]]['priorpolarity']=='positive'])
-n_words_fd=([w for w in f2_half if (w[0] in ws.keys()) and ws[w[0]]['priorpolarity']=='negative'])
+def get_score(p_words_fd,n_words_fd):
+	p=sum([w[1] for w in p_words_fd])
+	n=sum([w[1] for w in n_words_fd])
+	return (p-n)/float(p+n)
 
 
-import matplotlib.pyplot as plt
+def analyse(cname):
+	fname='reviews_'+cname+'.csv'
+	rs=csv2table(fname)
+	txt=[r['review'] for r in rs]
+	tagd=[nltk.pos_tag(nltk.word_tokenize(t)) for t in txt]
+	# tags=[tag for sl in tagd for (txt,tag) in sl]
+	# f1=nltk.FreqDist(tags)
+	jjs=[txt for sl in tagd for (txt,tag) in sl if tag==('JJ')]
+	f2=nltk.FreqDist(jjs)
+	ws={}
+	for item in kvpairs2table('words.tff'):
+	   word1 = item.pop('word1')
+	   ws[word1] = item
+	f2_half=f2.items()[0:getTopQuantileIdx(f2,0.5)]
+	p_words_fd=([w for w in f2_half if (w[0] in ws.keys()) and ws[w[0]]['priorpolarity']=='positive'])
+	n_words_fd=([w for w in f2_half if (w[0] in ws.keys()) and ws[w[0]]['priorpolarity']=='negative'])
+	print 'Score for fname:', get_score(p_words_fd,n_words_fd)
+	save_plot(p_words_fd,n_words_fd,cname)
 
-plt.bar(numpy.arange(len(p_words_fd)), [w[1] for w in p_words_fd], 1, color='g',alpha=0.25)
-plt.bar(numpy.arange(len(n_words_fd)), [w[1] for w in n_words_fd], 1, color='r',alpha=0.25)
-
-[plt.text(i,p_words_fd[i][1],p_words_fd[i][0]) for i in range(len(p_words_fd))]
-[plt.text(i,n_words_fd[i][1],n_words_fd[i][0]) for i in range(len(n_words_fd))]
-
-plt.show()
 
 
-p=sum([w[1] for w in p_words_fd])
-n=sum([w[1] for w in n_words_fd])
-score=(p-n)/float(p+n)
-
+# analyse('Mumbai')
+analyse('NewYork')
